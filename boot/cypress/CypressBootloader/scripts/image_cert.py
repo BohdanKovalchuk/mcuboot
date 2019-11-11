395202a05d5beed807d2a1aa245fd79d7b55f236
Bohdan Kovalchuk <bohd@cypress.com>
2019-11-11 12:10:22 +0200

    fixing toc3_crc script

--- boot/cypress/CypressBootloader/scripts/image_cert.py
+++ boot/cypress/CypressBootloader/scripts/image_cert.py
@@ -27,35 +27,54 @@ from jwcrypto import jwk, jwt
 from intelhex import IntelHex
 
 IMAGE_ID = 0
-IMAGE_TOC3_ADDR = int('0x100DFE00', 0)
 CERT_EXP = datetime(2019, 12, 31)
 IMAGE_ADDR_OFFST = 16
 IMAGE_SIZE_OFFST = 20
 IMAGE_VERSION_OFFST = 24
 IMAGE_BUILD_OFFS = 28
+WORD_SIZE = 4
+TOC3_MAGIC = 0x01211221
+TOC3_SIZE = 0x200-WORD_SIZE
 
-def calculate_image_hash(image, toc3_addr):
+def find_toc3(ih):
+    """
+    Look for TOC3 in the IntelHex image using its known magic and size
+    """
+    toc3_addr = 0
+    addrs = list(range(ih.minaddr(), ih.maxaddr(), WORD_SIZE))
+    for addr in addrs:
+        word_arr = ih.tobinarray(start=addr, size=WORD_SIZE)
+        word = struct.unpack('<I', word_arr)[0]
+        if word == TOC3_SIZE:
+            next_word_arr = ih.tobinarray(start=addr+WORD_SIZE, size=WORD_SIZE)
+            next_word = struct.unpack('<I', next_word_arr)[0]
+            if next_word == TOC3_MAGIC:
+                toc3_addr = addr
+                break
+
+    return toc3_addr
+
+def process_image(image):
     """
     Calculate image hash and read some metadata from the image
     """
     ih = IntelHex(image)
     ih.padding = 0x00
-    segments = ih.segments()
-    toc3_size_arr = ih.tobinarray(start=segments[1][0], size=4)
-    toc3_size = struct.unpack('<I', toc3_size_arr)[0]
 
-    image_addr_arr = ih.tobinarray(start=(segments[1][0] + IMAGE_ADDR_OFFST), size=4)
+    toc3_addr = find_toc3(ih)
+
+    image_addr_arr = ih.tobinarray(start=(toc3_addr + IMAGE_ADDR_OFFST), size=4)
     image_addr = struct.unpack('<I', image_addr_arr)[0]
 
-    image_size_arr = ih.tobinarray(start=(segments[1][0] + IMAGE_SIZE_OFFST), size=4)
+    image_size_arr = ih.tobinarray(start=(toc3_addr + IMAGE_SIZE_OFFST), size=4)
     image_size = struct.unpack('<I', image_size_arr)[0]
 
-    image_version = ih.tobinarray(start=(segments[1][0] + IMAGE_VERSION_OFFST), size=3)
-    image_build = ih.tobinarray(start=(segments[1][0] + IMAGE_BUILD_OFFS), size=2)
+    image_version = ih.tobinarray(start=(toc3_addr + IMAGE_VERSION_OFFST), size=3)
+    image_build = ih.tobinarray(start=(toc3_addr + IMAGE_BUILD_OFFS), size=2)
 
     sha256 = hashlib.sha256()
     sha256.update(ih.tobinarray(start=image_addr, size=image_size))
-    sha256.update(ih.tobinarray(start=toc3_addr, size=toc3_size))
+    sha256.update(ih.tobinarray(start=toc3_addr, size=TOC3_SIZE))
 
     image_hash = sha256.digest()
 
@@ -124,7 +143,7 @@ def main(image, key, img_cert):
     """
     Entry point of the script
     """
-    image_attr = calculate_image_hash(image, IMAGE_TOC3_ADDR)
+    image_attr = process_image(image)
     key_json = load_key(key)
     payload = create_cert_payload(image, image_attr, key_json, IMAGE_ID, CERT_EXP)
     create_certificate(payload, key_json, img_cert)
